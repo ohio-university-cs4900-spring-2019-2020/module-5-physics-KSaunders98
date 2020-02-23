5463ad6232eb9e0d712c1537aa8458d777d7ec29
KSaunders98 <kirksaunders12@gmail.com>
2020-02-23 11:30:23 -0500

    Cleaned up parts of WOPhysXActor

--- src/WOPhysXActor.cpp
+++ src/WOPhysXActor.cpp
@@ -36,110 +36,90 @@ void WOPhysXActor::pullFromPhysX()
 
 void WOPhysXActor::pushToPhysX()
 {
-    Mat4 mat = getDisplayMatrix();
-    Vector p = getPosition();
+    if (physxActor != nullptr) {
+        Mat4 mat = getDisplayMatrix();
+        Vector p = getPosition();
 
-    PxMat44 m;
-    for (unsigned int i = 0; i < 3; ++i) {
-        for (unsigned int j = 0; j < 3; ++j) {
-            m[i][j] = mat[i * 4 + j];
+        PxMat44 m;
+        for (unsigned int i = 0; i < 3; ++i) {
+            for (unsigned int j = 0; j < 3; ++j) {
+                m[i][j] = mat[i * 4 + j];
+            }
         }
+        m[3] = PxVec4(p.x, p.y, p.z, 1.0f);
+        physxActor->setGlobalPose(PxTransform(m));
     }
-    m[3] = PxVec4(p.x, p.y, p.z, 1.0f);
-    physxActor->setGlobalPose(PxTransform(m));
 }
 
 void WOPhysXActor::setPosition(const Vector& newXYZ)
 {
     WO::setPosition(newXYZ);
-
-    PxTransform t = physxActor->getGlobalPose();
-    t.p = PxVec3(newXYZ.x, newXYZ.y, newXYZ.z);
-    physxActor->setGlobalPose(t);
+    pushToPhysX();
 }
 
 void WOPhysXActor::setPosition(float x, float y, float z)
 {
     WO::setPosition(x, y, z);
-
-    PxTransform t = physxActor->getGlobalPose();
-    t.p = PxVec3(x, y, z);
-    physxActor->setGlobalPose(t);
+    pushToPhysX();
 }
 
 void WOPhysXActor::setPositionIgnoringAllChildren(const Vector& newXYZ)
 {
     WO::setPositionIgnoringAllChildren(newXYZ);
-
-    PxTransform t = physxActor->getGlobalPose();
-    t.p = PxVec3(newXYZ.x, newXYZ.y, newXYZ.z);
-    physxActor->setGlobalPose(t);
+    pushToPhysX();
 }
 
 void WOPhysXActor::moveRelative(const Vector& dXdYdZ)
 {
     WO::moveRelative(dXdYdZ);
-
-    PxTransform t = physxActor->getGlobalPose();
-    t.p += PxVec3(dXdYdZ.x, dXdYdZ.y, dXdYdZ.z);
-    physxActor->setGlobalPose(t);
+    pushToPhysX();
 }
 
 void WOPhysXActor::moveRelativeIgnoringAllChildren(const Vector& dXdYdZ)
 {
     WO::moveRelativeIgnoringAllChildren(dXdYdZ);
-
-    PxTransform t = physxActor->getGlobalPose();
-    t.p += PxVec3(dXdYdZ.x, dXdYdZ.y, dXdYdZ.z);
-    physxActor->setGlobalPose(t);
+    pushToPhysX();
 }
 
 void WOPhysXActor::rotateToIdentity()
 {
     WO::rotateToIdentity();
-
     pushToPhysX();
 }
 
 void WOPhysXActor::rotateAboutRelX(float deltaRadianAngle)
 {
     WO::rotateAboutRelX(deltaRadianAngle);
-
     pushToPhysX();
 }
 
 void WOPhysXActor::rotateAboutRelY(float deltaRadianAngle)
 {
     WO::rotateAboutRelY(deltaRadianAngle);
-
     pushToPhysX();
 }
 
 void WOPhysXActor::rotateAboutRelZ(float deltaRadianAngle)
 {
     WO::rotateAboutRelZ(deltaRadianAngle);
-
     pushToPhysX();
 }
 
 void WOPhysXActor::rotateAboutGlobalX(float deltaRadianAngle)
 {
     WO::rotateAboutGlobalX(deltaRadianAngle);
-
     pushToPhysX();
 }
 
 void WOPhysXActor::rotateAboutGlobalY(float deltaRadianAngle)
 {
     WO::rotateAboutGlobalY(deltaRadianAngle);
-
     pushToPhysX();
 }
 
 void WOPhysXActor::rotateAboutGlobalZ(float deltaRadianAngle)
 {
     WO::rotateAboutGlobalZ(deltaRadianAngle);
-
     pushToPhysX();
 }
 
